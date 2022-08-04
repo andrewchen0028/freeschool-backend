1e49b2f9d75cab452064486200e4eecda95fadb8
Andrew Chen <andrewchen0028@gmail.com>
2022-08-03 19:15:48 -0700

    split graph into nodes and links

--- src/index.js
+++ src/index.js
@@ -6,48 +6,61 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const graph = {
-  "nodes": [{ id: uuidv4(), title: "root", color: "#4285F4" }],
-  "links": []
-};
-const resources = {};
+const nodes = [
+  { id: uuidv4(), title: "node0", color: "#4285F4" },
+  { id: uuidv4(), title: "node1", color: "#4285F4" },
+  { id: uuidv4(), title: "node2", color: "#4285F4" }
+];
 
-app.get("/api/graph", (_request, response) => {
-  response.json(graph).status(200).end();
+const links = [
+  { id: uuidv4(), source: nodes[0].id, target: nodes[1].id },
+  { id: uuidv4(), source: nodes[1].id, target: nodes[2].id }
+];
+
+const resources = [
+  { id: uuidv4(), nodeId: nodes[0].id, title: "r0", url: "https://nsa.gov" },
+  { id: uuidv4(), nodeId: nodes[1].id, title: "r1", url: "https://fbi.gov" },
+  { id: uuidv4(), nodeId: nodes[2].id, title: "r2", url: "https://usa.gov" }
+];
+
+app.get("/graph", (_request, response) => {
+  response.json({ nodes: nodes, links: links }).status(200).end();
 });
 
-app.get("/api/nodes/:id/resources", (request, response) => {
+// TODO: deprecate after separating node page.
+app.get("/nodes/:id/resources", (request, response) => {
   response.json(resources[request.params.id]).status(200).end();
 });
 
-app.post("/api/graph/nodes", (request, response) => {
+app.post("/graph/nodes", (request, response) => {
   const newNode = {
     id: uuidv4(),
     title: request.body.title,
     color: "#4285F4"
   };
-  if (graph.nodes.some((node) => node.title === newNode.title)) {
+  if (nodes.some((node) => node.title === newNode.title)) {
     response.status(400).end();
   } else {
-    graph.nodes = graph.nodes.concat(newNode);
+    nodes = nodes.concat(newNode);
     response.json(newNode).status(200).end();
   }
 });
 
-app.post("/api/graph/links", (request, response) => {
+app.post("/graph/links", (request, response) => {
   const newLink = {
+    id: uuidv4(),
     source: request.body.source,
     target: request.body.target
   };
-  if (graph.links.some((link) => link === newLink)) {
+  if (links.some((link) => link === newLink)) {
     response.status(400).end();
   } else {
-    graph.links = graph.links.concat(newLink);
+    links = links.concat(newLink);
     response.json(newLink).status(200).end();
   }
 });
 
-app.post("/api/nodes/:id/resources", (request, response) => {
+app.post("/nodes/:id/resources", (request, response) => {
   const [nodeId, resourceId] = [request.params.id, uuidv4()];
   if (!resources[nodeId]) { resources[nodeId] = []; }
   resources[nodeId] = resources[nodeId].concat({
@@ -58,11 +71,11 @@ app.post("/api/nodes/:id/resources", (request, response) => {
   response.json(resources[nodeId][resourceId]).status(200).end();
 });
 
-app.delete("/api/graph/nodes/:id", (request, response) => {
+app.delete("/graph/nodes/:id", (request, response) => {
   const id = request.params.id;
-  graph.nodes = graph.nodes.filter(node => node.id !== id);
-  graph.links = graph.links.filter(link => link.source !== id);
-  graph.links = graph.links.filter(link => link.target !== id);
+  nodes = nodes.filter(node => node.id !== id);
+  links = links.filter(link => link.source !== id);
+  links = links.filter(link => link.target !== id);
   response.status(204).end();
 });
 
